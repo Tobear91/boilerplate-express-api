6a7ae791d848c7f524f035f95f759ebb54e9cf93
Camille HUrtaud <camille.hurtaud@gmail.com>
2025-07-15 20:40:47 +0200

    Ajout du timestamp dans les Schema Mongoose

--- models/users.js
+++ models/users.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  email: String,
-  password: String,
-});
+const userSchema = mongoose.Schema(
+  {
+    email: String,
+    password: String,
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("users", userSchema);
 
